28801318794ecba8a3332b504d2a48994bab74c6
Mariana <marianaplazas@live.com>
2019-05-03 19:06:22 +0000

    fix pep8

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     counter = 0
     all_ar = len(sys.argv)
-    for a in range (1, all_ar):
+    for a in range(1, all_ar):
         counter = int(sys.argv[a]) + counter
     print('{:d}'.format(counter))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     import hidden_4
     name = dir(hidden_4)
     for a in name:
-        if a[0] !='_':
+        if a[0] != '_':
             print("{}".format(a))
