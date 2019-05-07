ce3b364ca51b3f8bd7e9a287bb7fb8d0c5a682a7
Mariana <marianaplazas@live.com>
2019-05-07 22:20:30 +0000

    fix nine point

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,6 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return(None)
     else:
-        maxi = max(my_list)
+        my_list.sort()
+        maxi = my_list[-1]
         return(maxi)
