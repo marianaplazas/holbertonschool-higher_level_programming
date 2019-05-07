fe02424a872fa90e4096704d1228aadab2df1529
Mariana <marianaplazas@live.com>
2019-05-07 22:59:12 +0000

    8 point

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        tupla = 0, None
+        return(tupla)
+    else:
+        tupla = len(sentence), sentence[0]
+        return(tupla)
