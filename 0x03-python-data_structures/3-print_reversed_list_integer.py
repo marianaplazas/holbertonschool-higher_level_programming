f7302e335d6255f154bf49324826dc6ec749200b
Mariana <marianaplazas@live.com>
2019-05-08 02:37:07 +0000

    change if in 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) != 0:
+    if (my_list):
         for a in range(0, len(my_list)):
             print("{:d}".format(my_list[-1-a]))
-    else:
-        print(my_list)
