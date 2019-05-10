f30329905ae475addbfa35222cd27608cd1e5caa
Mariana <marianaplazas@live.com>
2019-05-10 00:04:13 +0000

    first review

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if (matrix):
+        new_matrix = [[x*x for x in row] for row in matrix]
+        return (new_matrix)
+    else:
+        return(matrix)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    newd = {}
+    for a in a_dictionary.keys():
+        newd[a] = (a_dictionary[a] * 2)
