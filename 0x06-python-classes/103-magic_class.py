62bb81e281ebac632c0ff2fae62a60324b964f72
Mariana <marianaplazas@live.com>
2019-05-22 03:34:36 +0000

    i delete the import and i dont know why :))) idiot

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import math
+
+
 class MagicClass:
     def __init__(self, radius):
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, int) and not isinstance(radius, float):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+        ((self.__radius * self.__radius) * math.pi)
 
     def circumference(self):
         return 2 * math.pi * self.__radius
