6ebf25db600abaee87d0f1a5a5e91c704a05561b
Mariana <marianaplazas@live.com>
2019-06-06 04:02:44 +0000

    add advance

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function to update
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    'update'
+    with open(filename, mode='r', encoding='utf-8') as f:
+        string = ""
+        for line in f:
+           if search_string in line:
+               string += line + new_string
+           else:
+               string += line
+        f.close()
+    with open(filename, mode='w') as f:
+        f.write(string)
