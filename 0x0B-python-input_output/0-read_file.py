9be43770089d16ba89745e29bd523f2c35465855
Mariana <marianaplazas@live.com>
2019-06-06 03:52:33 +0000

    Fix the bash

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """
-this module is for read a text file UFT8
+this module is for read a text file UFT8 with statement
 """
 
 
 def read_file(filename=""):
     'function that read a text file'
-    with open(filename, encoding='utf-8') as text:
-        print(text.read())
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
