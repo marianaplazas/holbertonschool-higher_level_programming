c8d8ee61192c7c00fa6f43b941da28975987d886
Mariana <marianaplazas@live.com>
2019-06-06 04:07:34 +0000

    pep8 advance

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,10 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, mode='r', encoding='utf-8') as f:
         string = ""
         for line in f:
-           if search_string in line:
-               string += line + new_string
-           else:
-               string += line
+            if search_string in line:
+                string += line + new_string
+            else:
+                string += line
         f.close()
     with open(filename, mode='w') as f:
         f.write(string)
