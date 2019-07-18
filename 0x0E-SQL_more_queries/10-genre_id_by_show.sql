338600f1a7d4e3ffc550b0d1917b78e4013f3ee3
Mariana <marianaplazas@live.com>
2019-07-18 04:30:31 +0000

    add advance and more corrections

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_shows_genres 
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id
+ORDER by tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- lists all shows, and all genres linked to that show
-SELECT tv_shows.title, tv_genres.name
-FROM tv_genres
-RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER by tv_shows.title;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_show_genres
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
