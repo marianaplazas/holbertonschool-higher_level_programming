9c5ba341857448aaf80d332648c59168de5a3e49
Mariana <marianaplazas@live.com>
2019-08-12 00:20:47 +0000

    add tre quotes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ selct the cities
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     name = sys.argv[1]
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     data_name = sys.argv[3]
     db = MySQLdb.connect(user=name, passwd=pwd, db=data_name)
     c = db.cursor()
-    c.execute("SELECT id, name FROM states")
+    c.execute("""SELECT id, name FROM states""")
     items = c.fetchall()
     for a in items:
         print(a)
