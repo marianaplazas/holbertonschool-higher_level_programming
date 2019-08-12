c22a51f0ebe74128e0b16a214ee7102827c45a3f
Mariana <marianaplazas@live.com>
2019-08-12 01:55:43 +0000

    change everything

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,15 +5,20 @@ user filter
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    name = sys.argv[1]
-    pwd = sys.argv[2]
-    data_name = sys.argv[3]
-    state = sys.argv[4]
-    db = MySQLdb.connect(user=name, passwd=pwd, db=data_name)
+if __name__ == '__main__':
+    """ code to be executed when imported """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],)
     c = db.cursor()
-    c.execute("""SELECT id, name FROM states WHERE name LIKE BINARY '{}' ORDER ASC""".format(argv[4]))
-    items = c.fetchall()
-    for a in items:
-        print(item)
+    c.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY"
+        " '{}' ORDER BY id ASC".format(argv[4]))
+    state_rows = cur.fetchall()
+    for state in state_rows:
+        print(state)
+    c.close()
     db.close()
