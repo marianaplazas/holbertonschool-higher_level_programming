4a6cb695a3a1fc934ce8ab58194387e468c1932f
Mariana <marianaplazas@live.com>
2019-09-18 03:57:58 +0000

    [fix] nine point

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
-
-function factorial (a) {
-  if (isNaN(a)) {
-    return (1);
-  } else if (a === 1) {
-    return (1);
-  } else {
-    return (a * factorial(a - 1));
-  }
-}
-
-console.log(factorial(process.argv.slice(2)[0]));
+function add (a, b) { console.log(a + b); }
+add(Number(process.argv[2]), Number(process.argv[3]));
