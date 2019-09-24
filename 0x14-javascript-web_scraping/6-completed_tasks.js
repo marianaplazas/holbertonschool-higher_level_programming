ac92aab05e4437818d7c221c915be5e5e15313af
Mariana <marianaplazas@live.com>
2019-09-24 23:39:22 +0000

    [fix] semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,16 +2,16 @@
 const request = require('request');
 const url = process.argv[2];
 request.get(url, function (error, response, body) {
-  let task = {};
+  const task = {};
   if (error) {
     return console.log(error);
   } else {
     for (let i = 0; i < JSON.parse(body).length; i++) {
       if (JSON.parse(body)[i].completed) {
-        let count = JSON.parse(body)[i].userId;
+        const count = JSON.parse(body)[i].userId;
         task[count] = (task[count] || 0) + 1;
       }
     }
   }
   console.log(task);
-}
+});
