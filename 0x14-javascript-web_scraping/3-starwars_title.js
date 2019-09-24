c2a28b42ad7c6b6acf6f32e941d13cde9793fa3d
Mariana <marianaplazas@live.com>
2019-09-24 20:21:48 +0000

    [fix] semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request.get(url, function(err, res, data){
-  console.log(JSON.parse(data)['title']);
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request.get(url, function (err, res, data) {
+  if (err);
+  console.log(JSON.parse(data).title);
 });
