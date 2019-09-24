04c2486b99b46f2bf4ad43e7c33f1178d2e9fc00
Mariana <marianaplazas@live.com>
2019-09-24 20:19:17 +0000

    [Fix] output 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request.get(url, function (data, error) {
-  console.log(JSON.parse(data));
+let request = require('request');
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request.get(url, function(err, res, data){
+  console.log(JSON.parse(data)['title']);
 });
