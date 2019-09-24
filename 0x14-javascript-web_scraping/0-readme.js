aa936eb504d3fd034aa15a6aab0d6054b344b1ca
Mariana <marianaplazas@live.com>
2019-09-24 21:00:57 +0000

    [fix] output 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-if (process.argv[2]) {
-  fs.readFile(process.argv[2], 'utf-8', function (file, err) {
-    err ? console.log(err) : console.log(file);
-  });
-}
+fs.readFile(process.argv[2], 'utf8', function (err, file) {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log(file);
+  }
+});
